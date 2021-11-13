010b650d8310778bcf162706bc90e9a1e3f705a2
Pedro-Ribas-Moreira <pedrohenryque.rm@gmail.com>
2021-11-13 19:45:04 +0000

    testing connection with vercel

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -75,7 +75,7 @@ const Login = (props) => {
 
   return (
     <Card className={classes.login}>
-      <h1>VAMO VAMO INTER!</h1>
+      <h1>VAMO VAMO INTER! gremio amargo! </h1>
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
